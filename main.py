a60cc0e6c9d9a5f12b70ef5bc93239efca1ab36b
Poloz Alexey <polozhev@mail.ru>
2017-07-30 02:36:09 +0300

    Pizdec

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from func import *
-import time, os 
+import time, os
+
+hello={'хай', 'привет', 'здарова', 'здаров', 'ghbdtn', '[fq', 'hay', 'hello', 'хело', 'хелло', 'хеллоу', 'хелоу', 'хей', 'здрасте', 'здрасть', 'здраст', 'эй', 'приветики', 'пивет', 'здравствуйте', 'здравствуй', 'здраствуй', 'здраствуйте', 'добро пожаловать', 'рад познакомиться', 'будем знакомы', 'хой', 'хеу', 'hi', 'helo'}
 
 first={'лево', '1', 'левая', 'левый', 'левой', 'один', 'первая', 'первый' 'первой', 'адин', 'одын', 'адын', 'лев', 'first', 'one', '1\'st', '1st', 'ван', 'left', 'фёрст', 'ферст'}
 
@@ -14,7 +16,8 @@ def how(cont):
 			cont[i]=' '
 
 	n=0
-	for i in str().join(cont).split():
+	text=str().join(cont).split()
+	for i in text:
 		if i in first:
 			if n!=2:
 				n=1
@@ -27,8 +30,12 @@ def how(cont):
 			else:
 				n=0
 				break
+	if n: return n
 
-	return n
+	for i in text:
+		if i in hello:
+			return 3
+	return 0
 
 #'Кто больше нравится?'
 kol=len(os.listdir('data'))
@@ -46,37 +53,40 @@ while True:
 		if nom>kol:
 			send(i[0], 'Ты уже всех оценил..')
 		else:
-			#send(i[0], '', [get(nom-1), get(nom)])
-
-			#b interval shift t j
-			if i[0] not in pe: pe[i[0]]=[nom, len(a), 0, 0, 0]
-
-			pe[i[0]][3]=pe[i[0]][1]%2
-			pe[i[0]][1]//=2
-			pe[i[0]][4]=(pe[i[0]][1])+pe[i[0]][3]
-
-			print(nom, get(int(pe[i[0]][4]+pe[i[0]][2]-1)))
-			send(i[0], '', [get(int(a[pe[i[0]][4]+pe[i[0]][2]-1])), get(nom)])
 			text=how(i[1].lower())
+			print(text, nom, pe)
+			if text in (1, 2):
+				#b interval shift t j
+				if i[0] not in pe: pe[i[0]]=[nom, len(a), 0, 0, 0]
 
-			if text==1:
-				pe[i[0]][2]+=pe[i[0]][4]
+				pe[i[0]][3]=pe[i[0]][1]%2
+				pe[i[0]][1]//=2
+				pe[i[0]][4]=(pe[i[0]][1])+pe[i[0]][3]
+
+				try:
+					send(i[0], '', [get(int(a[pe[i[0]][4]+pe[i[0]][2]-1])), get(nom)])
+				except ValueError:
+					print('Excess space')
+
+				if text==1:
+					pe[i[0]][2]+=pe[i[0]][4]
+				elif text==2:
+					if not pe[i[0]][3]: pe[i[0]][1]-=1
+
 				if pe[i[0]][1]<1:
 					a.append(nom)
 					for j in range(pe[i[0]][2]+1, len(a))[::-1]:
 						a[j], a[j-1]=a[j-1], a[j]
 					with open(name, 'w') as file:
 						for j in a:
-							print(j, file=file)
-			elif text==2:
-				if not pe[i[0]][3]: pe[i[0]][1]-=1
-				if pe[i[0]][1]<1:
-					a.append(nom)
-					for j in range(pe[i[0]][2]+1, len(a))[::-1]:
-						a[j], a[j-1]=a[j-1], a[j]
-					with open(name, 'w') as file:
-						for j in a:
-							print(j, file=file)
+							if j:
+								print(j, file=file)
+			elif text==3:
+				pe[i[0]]=[nom, len(a), 0, 0, 0]
+				pe[i[0]][3]=pe[i[0]][1]%2
+				pe[i[0]][1]//=2
+				pe[i[0]][4]=(pe[i[0]][1])+pe[i[0]][3]
+				send(i[0], '', [get(int(a[pe[i[0]][4]+pe[i[0]][2]-1])), get(nom)])
 			else:
 				send(i[0], 'Бред какой-то написал')
 	time.sleep(1)
